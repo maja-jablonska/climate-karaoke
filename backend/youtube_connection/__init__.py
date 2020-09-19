e49e3631f1fc5aea450b5310fd89e94924ccdcdc
Marta Pacuszka <pacuszka.m@gmail.com>
2020-09-20 00:52:14 +0200

    Finishing touches

--- backend/youtube_connection/__init__.py
+++ backend/youtube_connection/__init__.py
@@ -9,28 +9,27 @@ import os
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
+from typing import Sequence
 
 from .youtube_data import YoutubeData
 
 scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 
+# Disable OAuthlib's HTTPS verification when running locally.
+# *DO NOT* leave this option enabled in production.
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
+api_service_name = "youtube"
+api_version = "v3"
+
+# Get credentials and create an API client
+youtube = googleapiclient.discovery.build(
+    api_service_name, api_version, developerKey="AIzaSyDrQU83XoBHSbhfVyj8B0qtWNq9GyIy4bM")
 def fetch_video_data(query: str) -> YoutubeData:
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
-    api_service_name = "youtube"
-    api_version = "v3"
-
-    # Get credentials and create an API client
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey="AIzaSyDrQU83XoBHSbhfVyj8B0qtWNq9GyIy4bM")
-
     request = youtube.search().list(
         part="snippet",
         maxResults=25,
         q=query
     )
     response = request.execute()
-    return YoutubeData(response)
+    return YoutubeData(response, 0)
