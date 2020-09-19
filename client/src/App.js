e2ae8e30492520d02537fa69b2aa8439b8ff64f5
Marta Pacuszka <marta.pacuszka@cern.ch>
2020-09-19 18:59:12 +0200

    More frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,31 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import { Loading } from './Loading';
 import { Search } from './Search';
-
+import { Player } from './Player';
 
 function App() {
   const [value, setValue] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [playSong, setPlaySong] = useState(false)
+
+  useEffect(() => {
+    if (loading) {
+    const timer = setTimeout(() => {
+      setLoading(false)
+      setPlaySong(true)
+    }, 2000);
+    return () => clearTimeout(timer);
+  }
+  }, [loading]);
   return (
     <div className="App">
       <header className="App-header">
-        <Search value={value} setValue={setValue}/>
+        {loading ? <Loading setLoading={setLoading} setPlaySong={setPlaySong}/> :
+        <>
+        {playSong ? <Player setPlaySong={setPlaySong} /> :
+        <Search value={value} setValue={setValue} setLoading={setLoading}/>}
+</>}
       </header>
     </div>
   );
