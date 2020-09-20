dc77c9b493fc18f142b6a9cbb3aa5368388d11da
Maja Jabłońska <majajjablonska@gmail.com>
2020-09-20 05:15:59 +0200

    Add ssl

--- application.py
+++ application.py
@@ -5,4 +5,4 @@ application = create_app()
 
 
 if __name__ == '__main__':
-    application.run(threaded=True, host= '0.0.0.0')
+    application.run(threaded=True, host='0.0.0.0', ssl_context='adhoc')
